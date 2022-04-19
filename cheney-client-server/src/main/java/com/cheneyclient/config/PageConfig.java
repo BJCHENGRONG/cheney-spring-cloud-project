e8024b9ab4aacd8152e8b61ad2eb9771882f1c6e
CHENEY <bj_chengrong@163.com>
2022-04-19 17:10:58 +0800

    模块：微服务搭建 功能：新增knife4j插件 描述：新增knife4j插件配置

--- cheney-client-server/src/main/java/com/cheneyclient/config/PageConfig.java
+++ cheney-client-server/src/main/java/com/cheneyclient/config/PageConfig.java
@@ -1,7 +1,9 @@
 package com.cheneyclient.config;
 
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.cheneyclient.dao")
 public class PageConfig {
+    /**
+     * 新版本MySQL-Plus分页插件
+     */
     @Bean
-    public PaginationInterceptor PaginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
 
